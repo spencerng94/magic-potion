381d856e4646a54e1715e62643eb9ceb981cb423
Spencer Ng <spencerng94@gmail.com>
2021-04-20 17:25:26 -0700

    Removed bootstrap from css styling form

--- resources/js/components/Form.js
+++ resources/js/components/Form.js
@@ -11,11 +11,11 @@ class Form extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            quantity = 0,
-            total = 0,
-            email = '',
-            ccNum = '',
-            exp = ''
+            quantity: 0,
+            total: 0,
+            email: '',
+            ccNum: '',
+            exp: ''
         }
     }
 
