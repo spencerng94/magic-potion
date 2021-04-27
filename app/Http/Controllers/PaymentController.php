1fd4e97eb435b77c3f0f18e489503c87174df773
Spencer Ng <spencerng94@gmail.com>
2021-04-26 23:40:07 -0700

    Implemented database with MySQL

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -16,6 +16,13 @@ class PaymentController extends Controller
         $user->total = $request->total;
         $user->save();
 
+        // $user = new User([
+        //     'email' => $request->email,
+        //     'quantity' => $request->quantity,
+        //     'total' => $request->total
+        // ])
+        // $user->save();
+
         $ccNum = $request["payment"]["ccNum"];
         $exp = $request["payment"]["exp"];
 
@@ -29,6 +36,13 @@ class PaymentController extends Controller
         $payment->exp = $exp;
         $payment->save();
 
+        // $payment = new Payment([
+        //     'user_id' => $last,
+        //     'ccNum' => $ccNum,
+        //     'exp' => $exp
+        // ])
+        // $payment->save();
+
         $uid = Payment::latest()->first("id")->value("id");
 
         return response()->json([
@@ -36,3 +50,34 @@ class PaymentController extends Controller
         ], 201);
     }
 }
+
+// class PaymentController extends Controller
+// {
+//     public function createPayment(Request $request)
+//     {
+//         $user = new User;
+//         $user->email = $request->email;
+//         $user->quantity = $request->quantity;
+//         $user->total = $request->total;
+//         $user->save();
+
+//         $ccNum = $request["payment"]["ccNum"];
+//         $exp = $request["payment"]["exp"];
+
+//         // $ccNum = $request->payment->ccNum;
+//         // $exp = $request->payment->exp;
+//         $last = User::latest()->first("id")->value("id");
+
+//         $payment = new Payment;
+//         $payment->user_id = $last;
+//         $payment->ccNum = $ccNum;
+//         $payment->exp = $exp;
+//         $payment->save();
+
+//         $uid = Payment::latest()->first("id")->value("id");
+
+//         return response()->json([
+//             "id" => $uid
+//         ], 201);
+//     }
+// }
